9c20db65d1e41b86bf27e157b7f41cb5e745ff43
TomasUri <urinovic.tomas@gmail.com>
2022-05-25 08:23:46 +0300

    2022-05-24 homework

---
+++ 001/spalvotas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href = "./style.css">
+    <title>Spalvotas puslapis</title>
+</head>
+<body>
+    <button class = "btn"><a style = "text-decoration: none; padding: 17px 50px; color: black; font-size: 30px" href = "http://localhost/php-homework/2022-05-24/001/spalvotas.php">Black</a></button>
+    <button class = "btn"><a style = "text-decoration: none; padding: 17px 50px; color: red; font-size: 30px" href = "http://localhost/php-homework/2022-05-24/001/spalvotas.php?color=1">Red</a></button>
+    <?php
+    if($_GET["color"]) {
+    ?>
+    <body style = "background-color: red;">
+    <?php
+    }
+    ?>
+    <?php
+    if($_GET["color"] == null) {
+    ?>
+    <body style = "background-color: black;">
+    <?php
+    }
+    ?>
+</body>
+</html>
---
+++ 003/spalvotas003.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+   $_SESSION['switch'] = $_POST['color'];
+
+    header('Location: http://localhost/php-homework/2022-05-24/003/spalvotas003.php');
+    die;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+
+<body style="background-color: <?php echo $_SESSION['switch']?>">
+<?php
+    ?>
+    <form action="" method="post">
+    <span>Spalvos kodas:</span> <input type="text" name="color">
+    <button class="btn" type="submit">GO</button>
+    </form>
+</body>
+</html>
