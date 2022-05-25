4c047e0145e449bfa854ce262b1b55bf313d97dd
TomasUri <urinovic.tomas@gmail.com>
2022-05-25 13:09:30 +0300

    002

--- 002/002.php
+++ 002/002.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spalvotas puslapis</title>
+    <title>002</title>
 </head>
 <?php
     $color = $_GET["color"];
