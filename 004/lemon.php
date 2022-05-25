bff3ce722e26fa6b51afce734718656edf454c10
TomasUri <urinovic.tomas@gmail.com>
2022-05-25 08:37:33 +0300

    004

---
+++ 004/lemon.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+    header('Refresh: 4; http://localhost/php-homework/2022-05-24/004/orange.php');
+    
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spalvotas puslapis</title>
+</head>
+    <body style="background-color: #FAFA33">
+    </body>
+</html>
