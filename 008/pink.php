9f6218f8e1c1ab1df9f6329132c95582e6102650
TomasUri <urinovic.tomas@gmail.com>
2022-05-26 08:09:54 +0300

    008

---
+++ 008/pink.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href = "./style008.css">
+    <title>Pink</title>
+</head>
+<body style="background-color: #FF69B4">
+<form action=rose.php method="post">
+        <button type="submit" class = "btnRose">GO TO ROSE</button>
+    </form>
+</body>
+</html>
---
+++ 008/rose.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+ header('Location: http://localhost/php-homework/2022-05-24/008/pink.php');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href = "./style008.css">
+    <title>Rose</title>
+</head>
+<body style="background-color: #F33A6A">
+</body>
+</html>
